8406e7ad490311380664bb96e09cd7a2cc19758a
Buckle <matthew.r.buckle@gmail.com>
2015-02-28 10:51:12 -0500

    New python file to reverse a sentence

---
+++ split_example.py
@@ -0,0 +1,22 @@
+def reverse_sentence(old_sentence):
+	print old_sentence
+	new_sentence = ""
+	saved_index = 0
+	
+	for index in range(0,len(old_sentence)):
+		if old_sentence[index] == " ":
+			new_sentence = old_sentence[saved_index:index+1]+new_sentence
+			saved_index = index+1
+		elif index == len(old_sentence)-1:
+			new_sentence = old_sentence[saved_index:index+1]+" "+new_sentence
+	print new_sentence
+
+old_sentence = raw_input("Enter a sentence: ")
+reverse_sentence(old_sentence)
+
+split_sentence = old_sentence.split()
+print split_sentence
+new_sentence = ""
+for index in range(0,len(split_sentence)):
+	new_sentence = new_sentence + split_sentence[len(split_sentence)-index-1] + " "
+print new_sentence
